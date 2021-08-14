60518e9c18235d324d243ae4cc1ff12662156539
ManuDev03 <manuprajan03@gmail.com>
2021-08-14 15:47:45 +0530

    apply directive on host events

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef ,HostListener} from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
@@ -6,7 +6,21 @@ import { Directive, ElementRef } from '@angular/core';
 export class HighlightDirective {
 
   constructor(private el: ElementRef) { 
-    el.nativeElement.style.backgroundColor="yellow"
+    // el.nativeElement.style.backgroundColor="yellow"
   }
+
+  getHighlightColor(color:string){
+    this.el.nativeElement.style.backgroundColor= color
+  }
+
+  @HostListener('mouseenter')
+  onMouseEnter(){
+    this.getHighlightColor("orange")
+  }
+  @HostListener('mouseleave')
+  onMouseLeave(){
+    this.getHighlightColor("cyan")
+  }
+
 
 }
